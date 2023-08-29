5a785954214b2e3fc5b63c316b2faaf7afbfdcf0
LauraPlaza <0619967435@uma.es>
2023-08-29 13:34:09 +0200

    depuracion

--- numArchivos.py
+++
@@ -1,7 +0,0 @@
-import os
-
-dir_path = 'InfoOpenPose/json/frontalJson'
-files = os.listdir(dir_path)
-print(files)
-
-print("Número de archivos en la carpeta:", len(files))
--- zBorrar.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system('python borrarVideoDrive.py')
-os.system('python borrarCarpetaDrive.py')
-os.system('python borrarInfoCarpetasProyecto.py')
